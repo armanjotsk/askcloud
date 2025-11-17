702527410bc54f27561c4550a7baf97553697c3e
armanjotsk <armanjots136@gmail.com>
2025-11-17 19:15:22 +0100

    commit pt1-5

--- exercicis/pt1-5/outputs.tf
+++ exercicis/pt1-5/outputs.tf
@@ -1,32 +1,26 @@
 #ArmanjotSK outputs
 
-# Les ips públiques per les instàncies publicas
-output "public_instance_ips" {
-  description = "Adreces IP públiques de les instàncies públiques"
+output "public_ips" {
+  description = "IPs públiques de les instàncies"
   value       = [for instance in aws_instance.public_instances : instance.public_ip]
 }
 
-# IDs de les instàncies privadeass
-output "private_instance_ids" {
+output "private_ids" {
   description = "IDs de les instàncies privades"
   value       = [for instance in aws_instance.private_instances : instance.id]
 }
 
-# Nom del bucket S3 
-output "s3_bucket_name" {
-  description = "Nom del bucket S3 creat"
-  value       = aws_s3_bucket.optional_bucket[0].bucket
-  condition   = length(aws_s3_bucket.optional_bucket) > 0
-}
-
-# ID de la VPC
 output "vpc_id" {
-  description = "ID de la VPC creada"
+  description = "ID de la VPC"
   value       = aws_vpc.main.id
 }
 
-# ID del Security Group
-output "security_group_id" {
-  description = "ID del Security Group principal"
+output "sg_id" {
+  description = "ID del Security Group"
   value       = aws_security_group.main_sg.id
+}
+
+output "bucket_name" {
+  description = "Nom del bucket S3"
+  value       = aws_s3_bucket.optional_bucket[0].bucket
 }
