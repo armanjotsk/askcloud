ba6d540b712047df06306b8897383191f17f2b8e
armanjotsk <armanjots136@gmail.com>
2025-11-18 22:24:25 +0100

    pt1-5

--- exercicis/pt1-5/outputs.tf
+++ exercicis/pt1-5/outputs.tf
@@ -1,21 +1,32 @@
 #ArmanjotSK outputs
 
 output "public_ids" {
-  description = "IDs de les instàncies públiques"
-  value       = [for instance in aws_instance.public_instances : instance.id]
-}
-
-output "public_ips" {
-  description = "IPs públiques de les instàncies"
-  value       = [for instance in aws_instance.public_instances : instance.public_ip]
+  value = [for instancias in aws_instance.instance_public : instancias.id]
+  description = "Mosteramos el ID de las maquinas publicas"
 }
 
 output "private_ids" {
-  description = "IDs de les instàncies privades"
-  value       = [for instance in aws_instance.private_instances : instance.id]
+  value = [for instancias in aws_aws_instance.instance_private : instancias.id]
+    description = "Mostramos el ID de las maquinas privadas"
+}
+
+output "public_ips" {
+  value = [for i in aws_instance.instance_public : i.public_ip]
+  description = "Mostramos  las IPs publicas"
+}
+
+output "private_ips" {
+  value = [for i in aws_instance.instance_public : i.private_ip]
+  description = "Mostmos las IPs privadas"
+}
+
+output "private_ids" {
+  value = [for i in aws_instance.instance_private : i.private_ip]
+  description = " Mostramos las IPs de las maquinas que son privadas"
 }
 
 output "bucket_name" {
-  description = "Nom del bucket S3"
-  value       = aws_s3_bucket.optional_bucket[0].bucket
+value = var.create_s3_bucket ? aws_s3_bucket.optional[0].bucket : null
 }
+
+
